28588412ad21ef3d490267bef2fb2db22ecba605
CutyDina <103970174+cutydina@users.noreply.github.com>
2024-02-25 21:00:06 -0600

    Update SwipeMovementwithMouse.gd

--- SwipeMovementwithMouse.gd
+++ SwipeMovementwithMouse.gd
@@ -8,22 +8,28 @@ var swiping  = false
 var treeshold = 10
 
 func _process(_delta):
-	#Add "Press" on InputMap and select "MouseLeftButton" to make this work.
-	if Input.is_action_just_pressed("press"):
-		if !swiping:
-			swiping = true
-			startPos = get_global_mouse_position()
-			print("Start Position", startPos)
-	
-	if Input.is_action_pressed("press"):
-		if swiping:
-			curPos = get_global_mouse_position()
-			if startPos.distance_to(curPos) >= length:
-				if abs(startPos.y - curPos.y) <= treeshold:
-					print("Horizontal Swipe!")
-					swiping = false
-				elif abs(startPos.x - curPos.x) <= treeshold:
-					print("Vertical Swipe!")
-					swiping = false
-		else:
-			swiping = false
+    #Add "press" on InputMap and select "MouseLeftButton" to make this work.
+    if Input.is_action_just_pressed("press"):
+        if !swiping:
+            swiping = true
+            startPos = get_global_mouse_position()
+            print("Start Position", startPos)
+    
+    if Input.is_action_pressed("press"):
+        if swiping:
+            curPos = get_global_mouse_position()
+            var delta = curPos - startPos
+            if delta.length() >= length:
+                if abs(delta.y) > abs(delta.x):
+                    if delta.y > treeshold:
+                        print("Swipe Down!")
+                    elif delta.y < -treeshold:
+                        print("Swipe Up!")
+                else:
+                    if delta.x > treeshold:
+                        print("Swipe Right!")
+                    elif delta.x < -treeshold:
+                        print("Swipe Left!")
+                swiping = false
+        else:
+            swiping = false
